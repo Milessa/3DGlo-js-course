11f3008d60776d2e8ded072e2adbffaeb25077ec
Milessa <milessacroitor@gmail.com>
2024-02-18 13:52:40 +0200

    Урок 21

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import scroll from "./modules/scroll";
+import calcValidation from "./modules/calcValidation";
+import formsValidation from "./modules/formsValidation";
 
 timer("20 november 2021");
 menu();
 modal();
 scroll();
+calcValidation();
+formsValidation();
