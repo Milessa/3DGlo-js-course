3907b516a5a23de12335b8def740a3de9c1ad5cf
Milessa <milessacroitor@gmail.com>
2024-02-18 13:41:25 +0200

    Урок 20 - Усложненное задание

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import scroll from "./modules/scroll";
 
 timer("20 november 2021");
 menu();
 modal();
+scroll();
