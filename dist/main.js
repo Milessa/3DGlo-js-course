dc86b191a69cb817bef18027a7cb70015a4a10c9
Milessa <milessacroitor@gmail.com>
2024-02-18 13:24:51 +0200

    Урок 18

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("File one.js"),console.log("File two.js")})();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import fileOne from "./modules/one";
+import fileTwo from "./modules/two";
+
+fileOne();
+fileTwo();
